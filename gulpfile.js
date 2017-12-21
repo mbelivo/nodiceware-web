e041a9c5f112ed5b841f7ebace006e6702ba96d1
mbelivo <naenga@protonmail.com>
2017-12-21 14:10:12 +0100

    cleanup & whitespace fixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,38 @@
 var gulp = require('gulp');
+var babel = require('gulp-babel');
+var cssnext = require('postcss-cssnext');
+var postcss = require('gulp-postcss');
 var sourcemaps = require('gulp-sourcemaps');
 
 var paths = {
-	css: 'src/css/**/*.css',
-	js: 'src/js/**/*.js',
-	static: 'static',
-}
+    css: 'src/css/**/*.css',
+    js: 'src/js/**/*.js',
+    static: 'static',
+};
+
 
 gulp.task('css', function() {
-	var postcss = require('gulp-postcss');
-	var cssnext = require('postcss-cssnext');
-
-	return gulp.src(paths.css)
-		.pipe(sourcemaps.init())
-		.pipe(postcss([cssnext]))
-		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest(paths.static + '/css'));
+    return gulp.src(paths.css)
+        .pipe(sourcemaps.init())
+        .pipe(postcss([cssnext]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(paths.static + '/css'));
 });
+
 
 gulp.task('js', function() {
-	var babel = require('gulp-babel');
-
-	return gulp.src(paths.js)
-		.pipe(sourcemaps.init())
-		.pipe(babel({ presets: ['env'] }))
-		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest(paths.static + '/js'));
+    return gulp.src(paths.js)
+        .pipe(sourcemaps.init())
+        .pipe(babel({ presets: ['env'] }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(paths.static + '/js'));
 });
+
 
 gulp.task('watch', function() {
-	gulp.watch(paths.css, ['css']);
-	gulp.watch(paths.js, ['js']);
+    gulp.watch(paths.css, ['css']);
+    gulp.watch(paths.js, ['js']);
 });
+
 
 gulp.task('default', ['css', 'js']);
