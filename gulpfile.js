ba936e565feeb4e4e8d94e80496124d0f396ab86
mbelivo <naenga@protonmail.com>
2017-12-21 14:06:02 +0100

    add babel js preprocessor

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var sourcemaps = require('gulp-sourcemaps');
 
 var paths = {
 	css: 'src/css/**/*.css',
+	js: 'src/js/**/*.js',
 	static: 'static',
 }
 
@@ -13,12 +14,23 @@ gulp.task('css', function() {
 	return gulp.src(paths.css)
 		.pipe(sourcemaps.init())
 		.pipe(postcss([cssnext]))
-		.pipe(sourcemaps.write())
+		.pipe(sourcemaps.write('.'))
 		.pipe(gulp.dest(paths.static + '/css'));
+});
+
+gulp.task('js', function() {
+	var babel = require('gulp-babel');
+
+	return gulp.src(paths.js)
+		.pipe(sourcemaps.init())
+		.pipe(babel({ presets: ['env'] }))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest(paths.static + '/js'));
 });
 
 gulp.task('watch', function() {
 	gulp.watch(paths.css, ['css']);
+	gulp.watch(paths.js, ['js']);
 });
 
-gulp.task('default', ['css']);
+gulp.task('default', ['css', 'js']);
